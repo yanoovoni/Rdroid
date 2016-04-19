188e918551ebd6c63f0c99d394efbd7b0225bfa9
yanoovoni <yanooni@gmail.com>
2016-04-19 19:56:01 +0300

    added more tasks to phone

---
+++ Rdroid/app/src/main/java/com/yanoonigmail/rdroid/service/OSInterface.java
@@ -0,0 +1,92 @@
+package com.yanoonigmail.rdroid.service;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+/**
+ * Created by yanoo on 19-Apr-16.
+ */
+public class OSInterface {
+
+    public static String readFile(File file) {
+        FileInputStream fis = null;
+        try
+        {
+            fis = new FileInputStream(file);
+        }
+        catch (FileNotFoundException e) {e.printStackTrace();}
+        InputStreamReader isr = new InputStreamReader(fis);
+        BufferedReader br = new BufferedReader(isr);
+
+        String test;
+        int anzahl=0;
+        try
+        {
+            while ((test=br.readLine()) != null)
+            {
+                anzahl++;
+            }
+        }
+        catch (IOException e) {e.printStackTrace();}
+
+        try
+        {
+            fis.getChannel().position(0);
+        }
+        catch (IOException e) {e.printStackTrace();}
+
+        String[] array = new String[anzahl];
+
+        String line;
+        int i = 0;
+        try
+        {
+            while((line=br.readLine())!= null)
+            {
+                array[i] = line;
+                i++;
+            }
+        }
+        catch (IOException e) {e.printStackTrace();}
+        return Arrays.toString(array);
+    }
+
+    public static void saveFile(File file, String fileData) {
+        String[] data = fileData.split(System.getProperty("line.separator"));
+        FileOutputStream fos = null;
+        try
+        {
+            fos = new FileOutputStream(file);
+        }
+        catch (FileNotFoundException e) {e.printStackTrace();}
+        try
+        {
+            try
+            {
+                for (int i = 0; i<data.length; i++)
+                {
+                    fos.write(data[i].getBytes());
+                    if (i < data.length-1)
+                    {
+                        fos.write("\n".getBytes());
+                    }
+                }
+            }
+            catch (IOException e) {e.printStackTrace();}
+        }
+        finally
+        {
+            try
+            {
+                fos.close();
+            }
+            catch (IOException e) {e.printStackTrace();}
+        }
+    }
+}
--- Rdroid/app/src/main/java/com/yanoonigmail/rdroid/service/Task.java
+++ Rdroid/app/src/main/java/com/yanoonigmail/rdroid/service/Task.java
@@ -7,10 +7,16 @@ import android.util.Log;
 import com.yanoonigmail.rdroid.ApplicationContext;
 import com.yanoonigmail.rdroid.R;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Arrays;
 import java.util.logging.SocketHandler;
 
 import static com.yanoonigmail.rdroid.R.string.protocol_client_task_results_output_separator;
@@ -25,8 +31,8 @@ public class Task {
     private String type;
     private String parameters;
     private Thread taskThread;
-    private static Resources resources = ApplicationContext.getContext().getResources();
-    private Server server = Server.getInstance();
+    private final static Resources resources = ApplicationContext.getContext().getResources();
+    private final static Server server = Server.getInstance();
 
     public Task(String id, String type, String parameters) {
         this.id = id;
@@ -55,6 +61,12 @@ public class Task {
                     case "GET_FILES_IN_FOLDER":
                         output = getFilesInFolder(parameters.split(resources.getString(protocol_parameter_separator))[1]);
                         break;
+                    case "GET_FILE":
+                        output = getFile(parameters.split(resources.getString(protocol_parameter_separator))[1]);
+                        break;
+                    case "SAVE_FILE":
+                        String[] inputArray = parameters.split(resources.getString(protocol_parameter_separator), 1);
+                        output = saveFile(inputArray[0], inputArray[1]);
                 }
                 server.send(Protocol.taskResultsMessage(id, output));
             }
@@ -62,21 +74,66 @@ public class Task {
         taskThread.start();
     }
 
-    private String getFilesInFolder(String folder) {
+    private String getFilesInFolder(String folderLocation) {
         String filesString = "";
         File sdCardRoot = Environment.getExternalStorageDirectory();
-        File yourDir = new File(sdCardRoot, folder);
+        File yourDir = new File(sdCardRoot, folderLocation);
         for (File f : yourDir.listFiles()) {
             String name;
             if (f.isFile()) {
                 name = f.getName();
                 Log.i("file name", name);
+                filesString += name + resources.getString(protocol_client_task_results_output_separator);
             }
             else {
-                name = f.getName() + ":folder";
+                if (f.isDirectory()) {
+                    name = f.getName() + ":folder";
+                    filesString += name + resources.getString(protocol_client_task_results_output_separator);
+                }
             }
-            filesString += name + resources.getString(protocol_client_task_results_output_separator);
         }
         return filesString;
     }
+
+    private String getFile(String location) {
+        String output;
+        File sdCardRoot = Environment.getExternalStorageDirectory();
+        File theFile = new File(sdCardRoot, location);
+        if (!theFile.isFile()) {
+            output = "failure"  +
+                    resources.getString(protocol_client_task_results_output_separator) +
+                    "File not found";
+        }
+        else {
+            if (!theFile.canRead()) {
+                output = "failure"  +
+                        resources.getString(protocol_client_task_results_output_separator) +
+                        "Cannot read the file";
+            }
+            else {
+                output = "success" +
+                        resources.getString(protocol_client_task_results_output_separator) +
+                        OSInterface.readFile(theFile);
+            }
+        }
+        return output;
+    }
+
+
+
+    private String saveFile(String fileLocation, String fileData) {
+        String output;
+        try {
+            File sdCardRoot = Environment.getExternalStorageDirectory();
+            File theFile = new File(sdCardRoot, fileLocation);
+            OSInterface.saveFile(theFile, fileData);
+            output = "success";
+        } catch (Exception e) {
+            e.printStackTrace();
+            output = "failure";
+        }
+        return output;
+    }
+
+
 }
