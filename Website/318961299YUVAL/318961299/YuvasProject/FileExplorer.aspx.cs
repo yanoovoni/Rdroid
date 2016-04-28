cfc9742a9b982dc16b2f943f59709bd53b985d0c
yanoovoni <yanooni@gmail.com>
2016-04-28 20:54:12 +0300

    fixed some more shit

--- Website/318961299YUVAL/318961299/YuvasProject/FileExplorer.aspx.cs
+++ Website/318961299YUVAL/318961299/YuvasProject/FileExplorer.aspx.cs
@@ -9,9 +9,17 @@ public partial class FileExplorer : System.Web.UI.Page
 {
     private string Folder = "";
     private string[] FilesInFolder;
+
     protected void Page_Load(object sender, EventArgs e)
     {
-        Folder = "";
+        this.Folder = (string)Session["Folder"];
+        this.FilesInFolder = (string[])Session["FilesInFolder"];
+    }
+
+    protected void Page_Unload(object sender, EventArgs e)
+    {
+        Session["Folder"] = this.Folder;
+        Session["FilesInFolder"] = this.FilesInFolder;
     }
 
     protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
@@ -21,7 +29,7 @@ public partial class FileExplorer : System.Web.UI.Page
 
     public void PopulateGridView()
     {
-        FilesInFolder = Task.GetFilesInFolder("yuval5898@walla.co.il", Folder);
+        this.FilesInFolder = Task.GetFilesInFolder("yuval5898@walla.co.il", Folder);
         GridViewExplorer.DataSource = FilesInFolder;
         GridViewExplorer.DataBind();
     }
@@ -40,14 +48,32 @@ public partial class FileExplorer : System.Web.UI.Page
         if (e.CommandName == "chck")
         {
             int index = Convert.ToInt32(e.CommandArgument);
-            Folder += "\\" + FilesInFolder[index];
-            PopulateGridView();
+            string FileName = this.FilesInFolder[index];
+            if (FileName.EndsWith(":folder"))
+            {
+                int FolderNotifiyerIndex = FileName.LastIndexOf(":");
+                if (FolderNotifiyerIndex > 0)
+                {
+                    FileName = FileName.Substring(0, FolderNotifiyerIndex);
+                }
+                if (this.Folder != "")
+                {
+                    this.Folder += "/";
+                }
+                this.Folder += FileName;
+                PopulateGridView();
+            }
+            else
+            {
+                Task.GetFile("yuval5898@walla.co.il", this.Folder += "/" + FileName);
+                //todo send the output of this task back to the client.
+            }
         }
     }
 
     protected void ToPreviousFolder()
     {
-        int index = Folder.LastIndexOf("\\");
+        int index = Folder.LastIndexOf("/");
         if (index > 0)
         {
             Folder = Folder.Substring(0, index);
